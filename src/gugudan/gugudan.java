41abe9f18489c9e1b7a83bb762a7744baa3df60a
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-17 14:52:12 +0900

    반복문까지학

---
+++ src/gugudan/gugudan.java
@@ -0,0 +1,93 @@
+
+package gugudan;
+import java.util.Scanner;
+
+public class gugudan {
+public static void main(String[]args) {
+	
+	
+	
+	int i = 1;
+	while ( i < 10 ) { 
+	System.out.println( 6 * i );
+	i = i+1;
+	}
+	
+	//7단
+	for(int j = 1 ; j < 10 ; j++) {
+			System.out.println(7 * j);
+	}
+	
+	//연습		
+	for(int j = 1 ; j<10 ; j++) {
+		System.out.println( 8 * j );
+	}
+	 
+	
+//	// 2단
+//	SYSTEM.OUT.PRINTLN("2단");
+//	SYSTEM.OUT.PRINTLN(2*1);
+//	SYSTEM.OUT.PRINTLN(2*2);
+//	SYSTEM.OUT.PRINTLN(2*3);
+//	SYSTEM.OUT.PRINTLN(2*4);
+//	SYSTEM.OUT.PRINTLN(2*5);
+//	System.out.println(2*6);
+//	System.out.println(2*7);
+//	System.out.println(2*8);
+//	System.out.println(2*9);
+//	System.out.println(2*10);
+//	
+//	//  3단
+//	System.out.println("3단");
+//	System.out.println(3*1);
+//	System.out.println(3*2);
+//	System.out.println(3*3);
+//	System.out.println(3*4);
+//	System.out.println(3*5);
+//	System.out.println(3*6);
+//	System.out.println(3*7);
+//	System.out.println(3*8);
+//	System.out.println(3*9);
+//	System.out.println(3*10);
+//	
+//	System.out.println("4단");
+//	
+//	int a = 4*1;
+//	System.out.println(a);
+//	a = 4*2;
+//	System.out.println(a);
+//    a = 4*3;
+//	System.out.println(a);
+//    a = 4*4;
+//	System.out.println(a);
+//	a = 4*5;
+//	System.out.println(a);
+//	a = 4*6;
+//	System.out.println(a);
+//	a = 4*7;
+//	System.out.println(a);
+//	a = 4*8;
+//	System.out.println(a);
+//	a = 4*9;
+//	System.out.println(a);
+//	
+//	System.out.println("값 입력 및 변수 ");
+//	System.out.println("출력할 구구단 단 수는 : ");
+//	Scanner scanner = new Scanner(System.in);
+//	int number = scanner.nextInt();	
+//	System.out.println(number * 1);
+//	System.out.println(number * 2);
+//	System.out.println(number * 3);
+//	System.out.println(number * 4);
+//	System.out.println(number * 5);
+//	System.out.println(number * 6);
+//	System.out.println(number * 7);
+//	System.out.println(number * 8);
+//	System.out.println(number * 9);
+//	
+	
+	
+}
+
+
+}
